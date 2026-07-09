10fd69a8e947f2d656749e8c587911147b467722
JosueRamirez1116 <josueramirez1116@gmail.com>
2026-07-09 15:45:25 -0600

    Actualizando SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 android {
     namespace = "com.example.facedetectionydetecciondemallafacial"
 
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.facedetectionydetecciondemallafacial"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
